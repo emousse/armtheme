5a2b60890891dea3426c2bb70958eb438c775cf1
emousse <etienne.mousse@gmail.com>
2017-01-11 14:32:20 +0100

    fix z-index header, site-content padding-top

--- js/app.js
+++ js/app.js
@@ -8,4 +8,35 @@ jQuery( document ).ready( function( $ ) {
   $('#site-cache').click(function(e){
     $('body').removeClass('with--sidebar');
   });
+
+  //"quantité" de scroll vertical
+  function scrollY(){
+    var supportPageOffset = window.pageXOffset !== undefined;
+var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+  }
+
+  var element  = document.querySelector('.header');
+  var siteContent = document.querySelector('.site-content');
+  var rect = element.getBoundingClientRect();
+  //position de l'element "top" par rapport au haut de la fenetre.
+  var top = rect.top + scrollY();
+
+  var fake = document.createElement('div');
+  fake.style.width = rect.width + "px";
+  fake.style.height = rect.height +"px";
+
+  var onScroll = function() {
+    var hasScrollClass = element.classList.contains('fixed');
+    if(scrollY()>top && !hasScrollClass){
+      element.classList.add('fixed')
+      element.style.width = rect.width + "px";
+      siteContent.style.paddingTop = rect.height + "px";
+    } else if (scrollY()<top && hasScrollClass){
+      element.classList.remove('fixed');
+      siteContent.style.paddingTop = "";
+    }
+  }
+  window.addEventListener('scroll', onScroll);
 } );
