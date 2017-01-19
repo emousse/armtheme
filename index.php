318d5e449c08375cf262269ef838ea54b5a23db4
emousse <etienne.mousse@gmail.com>
2017-01-19 17:37:15 +0100

    the loop

--- index.php
+++ index.php
@@ -51,22 +51,22 @@
                 <h3>Parquets</h3>
             </div>
             <div class="projects-item cuisine">
-                <h3>Parquets</h3>
+                <h3>Cuisine</h3>
             </div>
             <div class="projects-item cuisine">
-                <h3>Parquets</h3>
+                <h3>Cuisine</h3>
             </div>
             <div class="projects-item cuisine">
-                <h3>Parquets</h3>
+                <h3>Cuisine</h3>
             </div>
             <div class="projects-item renovation">
-                <h3>Parquets</h3>
+                <h3>Renovation</h3>
             </div>
             <div class="projects-item renovation">
-                <h3>Parquets</h3>
+                <h3>Renovation</h3>
             </div>
             <div class="projects-item renovation">
-                <h3>Parquets</h3>
+                <h3>Renovation</h3>
             </div>
         </div>
     </div>
@@ -86,6 +86,41 @@
 <div class="container">
     <h1>Who are you, my warranty?!</h1>
 
+    <?php if ( have_posts() ) : ?>
+
+        <?php if ( is_home() && ! is_front_page() ) : ?>
+            <header>
+                <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+            </header>
+        <?php endif; ?>
+
+        <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+
+            /*
+             * Include the Post-Format-specific template for the content.
+             * If you want to override this in a child theme, then include a file
+             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+             */
+            get_template_part( 'template-parts/content', get_post_format() );
+
+            // End the loop.
+        endwhile;
+
+        // Previous/next page navigation.
+        the_posts_pagination( array(
+            'prev_text'          => __( 'Previous page', 'twentysixteen' ),
+            'next_text'          => __( 'Next page', 'twentysixteen' ),
+            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
+        ) );
+
+    // If no content, include the "No posts found" template.
+    else :
+        get_template_part( 'template-parts/content', 'none' );
+
+    endif;
+    ?>
 
     <a href="#" class="blue-btn">Button</a>
     <a href="#" class="blue-btn">Button</a>
